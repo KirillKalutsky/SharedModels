e45fb16ed2e7948a3b08a08d3a42ee490c501100
KirillKalutsky <kefirchik.2000@mail.ru>
2022-01-04 12:06:50 +0500

    init

---
+++ SharedModels/Address.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Parser
+{
+    public class Address
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string AddressName { get; set; }
+        public District District { get; set; }
+    }
+}
---
+++ SharedModels/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SharedModels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
